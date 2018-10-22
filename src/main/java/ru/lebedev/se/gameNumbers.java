3dac7bc468adc604528a907a99c51d3c1a4f219e
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-10-22 18:31:28 +0300

    Завершена игра с цифрами

--- src/main/java/ru/lebedev/se/gameNumbers.java
+++ src/main/java/ru/lebedev/se/gameNumbers.java
@@ -14,9 +14,10 @@ public class gameNumbers {
         boolean willgame = true;
         do {
             System.out.println("Здравствуйте. Ваша задача угадать число от 0 до 9 с трех попыток");
-            int range = 9;
-            int number = (int) (Math.random() * range);
-            boolean win = false;
+            int range = 9; // диапазон чисел
+            int number = (int) (Math.random() * range); // случайно сгенерируемое число в диапазоне
+            int yesOrNo = 5; // Повторяем ли игру? 1 - да, 0 - нет. Для хотя бы одного запуска цикла делаем любым целым больше 1
+            boolean win = false; // Победили или нет
             for (int i = 1; i <= 3; i++) {
                 System.out.println("Введите число: ");
                 int input_number = scanner.nextInt();
@@ -33,25 +34,41 @@ public class gameNumbers {
             if (!win) {
                 System.out.println("К сожалению, вы не угадали число за 3 попытки и проиграли :(");
             }
-            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
-            boolean doItAgain = false;
-            do {
-                int yesOrNo = scanner.nextInt();
-                switch (yesOrNo) {
-                    case 1:
-                        willgame = true;
-                        doItAgain = false;
-                        break;
-                    case 0:
-                        willgame = false;
-                        doItAgain = false;
-                        break;
-                    default:
-                        System.out.println("Для новой игры нужно ввести 1, для выхода - 0, попробуйте еще раз");
-                        doItAgain = true;
-                }
+//            Данный код работает, но пробую сделать попроще
+//            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+//            boolean doItAgain = false;
+//            do {
+//                int yesOrNo = scanner.nextInt();
+//                switch (yesOrNo) {
+//                    case 1:
+//                        willgame = true;
+//                        doItAgain = false;
+//                        break;
+//                    case 0:
+//                        willgame = false;
+//                        doItAgain = false;
+//                        break;
+//                    default:
+//                        System.out.println("Для новой игры нужно ввести 1, для выхода - 0, попробуйте еще раз");
+//                        doItAgain = true;
+//                }
+//            }
+//            while (doItAgain);
+
+            while (!(yesOrNo >= 0 && yesOrNo <= 1)) {
+                System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+                yesOrNo = scanner.nextInt();
             }
-            while (doItAgain);
+            switch (yesOrNo) {
+                case 1:
+                    willgame = true;
+                    break;
+                case 0:
+                    willgame = false;
+                    break;
+                default:
+                    System.out.println("Странно, вы не должны были это увидеть");
+            }
         }
         while (willgame);
         System.out.println("До свидания, спасибо за игру");
