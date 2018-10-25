2a1cecc7d473ea9a88b5322d66a45eaa9137a2ac
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-10-25 15:31:22 +0300

    .

--- src/test/java/ru/lebedev/se/CrossAndZeroesTest.java
+++ src/test/java/ru/lebedev/se/CrossAndZeroesTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple CrossAndZeroes.
  */
-public class AppTest 
+public class CrossAndZeroesTest
 {
     /**
      * Rigorous Test :-)
