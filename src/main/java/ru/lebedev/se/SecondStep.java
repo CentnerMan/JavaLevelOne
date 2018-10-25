7b7d3b1903d7c46c01ec21164670d2b63c5b072a
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-10-25 10:06:04 +0300

    Renamed for JNC

--- src/main/java/ru/lebedev/se/SecondStep.java
+++ src/main/java/ru/lebedev/se/SecondStep.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  * @version 1.0.1
  */
 
-public class lesson2 {
+public class SecondStep {
     public static void main(String[] args) {
 
         // 1. Проверяем инвертирование массива
--- src/test/java/ru/lebedev/se/AppTest.java
+++ src/test/java/ru/lebedev/se/AppTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple lesson2.
+ * Unit test for simple SecondStep.
  */
 public class AppTest 
 {
