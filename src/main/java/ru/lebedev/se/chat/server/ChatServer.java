03b1a76e4c548b1da736b11c5ce2d3ce484de0ef
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-11-03 01:09:16 +0300

    final

--- src/main/java/ru/lebedev/se/chat/server/ChatServer.java
+++ src/main/java/ru/lebedev/se/chat/server/ChatServer.java
@@ -1,4 +1,16 @@
 package ru.lebedev.se.chat.server;
 
+import ru.lebedev.se.chat.server.api.ChatService;
+import ru.lebedev.se.chat.server.service.ChatServiceBean;
+
+import javax.xml.ws.Endpoint;
+
 public class ChatServer {
+
+    public static void main(String[] args) {
+        final ChatService service = new ChatServiceBean();
+        service.register("admin", "admin");
+        service.register("test", "test");
+        Endpoint.publish("http://localhost:8080/ChatService?wsdl",service);
+    }
 }
