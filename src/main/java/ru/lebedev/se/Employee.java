c1af43208300f3fae362075dd213d3cfd0278a3d
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-11-04 23:36:01 +0300

    final edition

--- src/main/java/ru/lebedev/se/Employee.java
+++ src/main/java/ru/lebedev/se/Employee.java
@@ -1,19 +1,25 @@
 package ru.lebedev.se;
 
+
 public class Employee {
     private String fio;
     private String position;
-    private String email = "ouroffice@ourwork.com"; // Если нет своей электронной почты, ставим рабочую
-    private String phoneNumber = "+78122233322"; // Если нет телефона - ставим рабочий
+    private String email;
+    private String phoneNumber;
     private int salary;
     private int age;
+    private final static String EMAIL = "ouroffice@ourwork.com"; // Если нет своей электронной почты, ставим рабочую
+    private final static String PHONE = "+78122233322"; // Если нет телефона - ставим рабочий
+
+    //------------------------------------------------------------------------------------------------------------
+    // Формируем простые сеттеры и геттеры
 
     public void setFio(String fio) {
         this.fio = fio;
     }
 
     public String getFio() {
-        return fio;
+        return fio.toUpperCase(); // Выводим в верхнем регистре
     }
 
     public void setPosition(String position) {
@@ -21,7 +27,7 @@ public class Employee {
     }
 
     public String getPosition() {
-        return position;
+        return toTitleCase(position); // Выводим строчными буквами с первой заглавной
     }
 
     public void setEmail(String email) {
@@ -52,12 +58,15 @@ public class Employee {
         if (age > 0)
             this.age = age;
         else
-            this.age = age *(-1); // Переводим отрицательный возраст в положительный
+            this.age = age * (-1); // Переводим отрицательный возраст в положительный, считаем опечаткой
     }
 
     public int getAge() {
         return age;
     }
+
+    //------------------------------------------------------------------------------------------------------------
+    // формируем конструкторы
 
     public Employee(String fio, String position, String email, String phoneNumber, int salary, int age) {
         setFio(fio);
@@ -69,26 +78,37 @@ public class Employee {
     }
 
     public Employee(String fio, String position, String phoneNumber, int salary, int age) {
-        setFio(fio);
-        setPosition(position);
-        setPhoneNumber(phoneNumber);
-        setSalary(salary);
-        setAge(age);
+        this(fio, position, EMAIL, phoneNumber, salary, age);
     }
 
     public Employee(String fio, String position, int salary, int age) {
-        setFio(fio);
-        setPosition(position);
-        setSalary(salary);
-        setAge(age);
+        this(fio, position, EMAIL, PHONE, salary, age);
     }
+
+    //------------------------------------------------------------------------------------------------------------
+    // Преобразуем первый символ строки в верхний регистр, остальные в нижний
+
+    public static String toTitleCase(String s) {
+        if (s.isEmpty()) {
+            return s;
+        }
+        return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+    }
+
+    //------------------------------------------------------------------------------------------------------------
+    // Выводим данные работника с помощью геттеров для форматирования
 
     public void printInfo() {
-        System.out.println("Ф.И.О.: " + this.fio + ", Должность: " + this.position + ", Электронная почта: "
-                + this.email + ", Телефон: " + this.phoneNumber + ", Зарплата: " + this.salary
-                + ", Возраст: " + this.age);
+        System.out.println("Ф.И.О.: " + getFio() + ", Должность: " + getPosition() + ", Электронная почта: "
+                + getEmail() + ", Телефон: " + getPhoneNumber() + ", Зарплата: " + getSalary()
+                + ", Возраст: " + getAge());
     }
 
+    // Выводим данные работника как введено
+    public void printInfoOriginal() {
+        System.out.println("Ф.И.О.: " + fio + ", Должность: " + position + ", Электронная почта: "
+                + email + ", Телефон: " + phoneNumber + ", Зарплата: " + salary + ", Возраст: " + age);
+    }
 }
 
 
