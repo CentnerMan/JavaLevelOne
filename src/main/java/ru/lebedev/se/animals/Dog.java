3c60efbb23e83dace496530d616570cf3dcc3182
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-11-10 14:47:19 +0300

    6 лекция

--- src/main/java/ru/lebedev/se/animals/Dog.java
+++ src/main/java/ru/lebedev/se/animals/Dog.java
@@ -1,4 +1,18 @@
 package ru.lebedev.se.animals;
 
-public class Dog {
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+        maxJumpHigh = 0.5 + Math.random() * 0.5; // jump from 0.5 to 1 meters
+        maxRunDistance = 400 + Math.random() * 200; // runs min 400 max 600 meters
+        maxSwimDistance = 5 + Math.random() * 10; // swim from 5 to 15 meters
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Собака: " + name + ", бегает до: " + roundTwo(maxRunDistance) +
+                " м., прыгает до: " + roundTwo(maxJumpHigh) + " м., плавает до: " + roundTwo(maxSwimDistance) + " м.");
+    }
+
 }
