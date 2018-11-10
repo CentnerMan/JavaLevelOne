797da7548da0549ed5cbad0304408bb6ee35b02f
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-11-10 14:46:35 +0300

    Переименовано

---
+++ src/main/java/ru/lebedev/se/HelloWorld.java
@@ -0,0 +1,14 @@
+package ru.lebedev.se;
+
+/**
+ * @author Анатолий Лебедев
+ * @version 1.0.0
+ */
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+    }
+
+}
