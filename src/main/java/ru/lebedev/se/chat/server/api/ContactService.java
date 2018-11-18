8d29ddf44138c0197d19e1402a4b7a60cefff866
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-11-19 01:08:37 +0300

    Contact Service

--- src/main/java/ru/lebedev/se/chat/server/api/ContactService.java
+++ src/main/java/ru/lebedev/se/chat/server/api/ContactService.java
@@ -1,4 +1,15 @@
 package ru.lebedev.se.chat.server.api;
 
-public interface ContactServise {
+import ru.lebedev.se.chat.server.model.Contact;
+import ru.lebedev.se.chat.server.model.ContactBox;
+
+public interface ContactService {
+
+    ContactBox contacts(String login);
+
+    Contact create(String source, String target);
+
+    void remove(String source, String target);
+
+    void removeAll(String login);
 }
