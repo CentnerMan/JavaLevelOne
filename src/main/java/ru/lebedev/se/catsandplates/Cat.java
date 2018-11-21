9dce72fe3d218ec82f5188b8572fca999866eda1
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-11-22 00:33:24 +0300

    Cats and Plates

--- src/main/java/ru/lebedev/se/catsandplates/Cat.java
+++ src/main/java/ru/lebedev/se/catsandplates/Cat.java
@@ -1,4 +1,22 @@
 package ru.lebedev.se.catsandplates;
 
 public class Cat {
+    private String name;
+    private int appetite;
+    private boolean satiety = false;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate plate) {
+        if (plate.decreaseFood(appetite)) satiety = true;
+    }
+
+    public void info() {
+        if (satiety) {
+            System.out.println("Кот " + name + " сыт");
+        } else System.out.println("Кот " + name + " голоден");
+    }
 }
--- src/main/java/ru/lebedev/se/catsandplates/Plate.java
+++ src/main/java/ru/lebedev/se/catsandplates/Plate.java
@@ -1,4 +1,27 @@
 package ru.lebedev.se.catsandplates;
 
 public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public boolean decreaseFood(int n) {
+        if (n <= food) {
+            food -= n;
+            return true;
+        }
+        return false;
+    }
+
+    public void increaseFood(int n) {
+        if (n > 0) {
+            food += n;
+        }
+    }
+
+    public void info() {
+        System.out.println("Еды в тарелке: " + food);
+    }
 }
